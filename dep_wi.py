d2bb9aeded2a8d7ac65acde15df3aeb56aad7c83
Pradnya Gaikwad <77426328+PradnyaSG@users.noreply.github.com>
2021-11-22 15:01:00 +0530

    Update dep_wi.py

--- dep_wi.py
+++ dep_wi.py
@@ -3,7 +3,44 @@ import main
 import sqlite3 as sql
 main.data_creation()
 connect = sql.connect("Bank.sqlite", check_same_thread=False)
-
+def deposit():
+    cur=connect.cursor()
+    with st.form("deposit_form1"):   
+        id=st.number_input("Enter the id of the Customer")
+        acc=st.number_input("Enter the account no of the Customer")
+        amt=st.number_input("Enter the amount to deposit")
+        deposit_cust_form25 = st.form_submit_button("submit")
+        if deposit_cust_form25 :
+            cur.execute("SELECT ACC_BAL FROM account WHERE CUST_ID = ?", (id,))
+            row = cur.fetchone()
+            amount=row[0]
+            if row:
+                amount=amount+amt
+                cur.execute("UPDATE account  SET  ACC_BAL=? WHERE CUST_ID = ?", (amount,id,))
+                cur.connection.commit()
+                st.write("Amount has been deposited.Below is the balance")
+                st.write("Rs",amount)
+            else:
+                st.write("Account does not exist")
+def withdraw():
+    cur=connect.cursor()
+    with st.form("withdraw_form1"):   
+        id=st.number_input("Enter the id of the Customer")
+        acc=st.number_input("Enter the account no of the Customer")
+        amt=st.number_input("Enter the amount to withdraw")
+        withdraw_cust_form2 = st.form_submit_button("submit")
+        if withdraw_cust_form2:
+            cur.execute("SELECT ACC_BAL FROM account WHERE CUST_ID = ?", (id,))
+            row = cur.fetchone()
+            amount=row[0]
+            if amount-amt>=5000:
+                amount=amount-amt
+                cur.execute("UPDATE account  SET  ACC_BAL=? WHERE CUST_ID = ?", (amount,id,))
+                cur.connection.commit()
+                st.write("Amount has been withdrawn.Below is the balance")
+                st.write("Rs",amount)
+            else:
+                 st.write("Dear Customer,You do not have sufficient balance to withdraw")
 def add_acount():
     cur=connect.cursor()
     with st.form("dept_form1"):
